b0a9bc1bd8276241d24d5fe25ad780a01a99b68d
karlssonevelina <89128496+karlssonevelina@users.noreply.github.com>
2022-04-29 11:14:41 +0200

    initial setup

---
+++ popup.js
@@ -0,0 +1,21 @@
+import { serverInit } from './server.js';
+
+// When the button is clicked, inject setPageBackgroundColor into current page
+playVideo.addEventListener("click", async () => {
+  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    function: clickPlayOrPause,
+  });
+
+  serverInit();
+});
+
+// The body of this function will be execuetd as a content script inside the
+// current page
+function clickPlayOrPause() {
+  chrome.storage.sync.get(() => {
+    document.querySelector("#react-mount > div > div.vask-container.Player-player-YFhdD > div > div > div.scene").click();
+  });
+}
