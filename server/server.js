9469758267ac860c0f9cc6e338d6fc028a542444
karlssonevelina <89128496+karlssonevelina@users.noreply.github.com>
2022-05-24 14:50:50 +0200

    added nicknames

--- server/server.js
+++ server/server.js
@@ -2,6 +2,9 @@
 const http = require('http');
 const WebSocketServer = require('websocket').server;
 
+var clients = [];
+var names = ["Tiger", "Panda", "Ant", "Elephant", "Giraffe", "Alligator"];
+
 const server = http.createServer();
 server.listen(9898);
 
@@ -11,10 +14,16 @@ const wsServer = new WebSocketServer({
 
 wsServer.on('request', function(request) {
     const connection = request.accept(null, request.origin);
+    let clientName = names[Math.floor(Math.random() * 6)];
+    clients.push({clientName: clientName, connection: connection});
 
     connection.on('message', function(message) {
-      console.log('Received Message:', message.utf8Data);
-      connection.sendUTF('Hi this is WebSocket server!');
+      console.log('Received Message From Client:', message.utf8Data);
+      console.log(message)
+
+      clients.forEach(function(client) {
+        client.connection.sendUTF(`${client.clientName}: `+ message.utf8Data);
+      })
     });
     connection.on('close', function(reasonCode, description) {
         console.log('Client has disconnected.');
